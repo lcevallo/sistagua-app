75a142637ce17ba83753a0e5f113924ddb89e19e
ccerezo <ccerezo90@gmail.com>
2021-02-11 16:10:30 -0500

    inteerface de cliente natural
    metodo que recoge todos los campos del formulario para crear cliente

--- src/app/modules/clientes/cliente-natural/cliente-natural.component.ts
+++ src/app/modules/clientes/cliente-natural/cliente-natural.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { IclienteNatural } from '@data/interfaces/icliente-natural';
 import { ProvinciasService } from '@data/services/api/provincias.service';
 
 @Component({
@@ -13,6 +14,7 @@ export class ClienteNaturalComponent implements OnInit {
   clienteFormGroup: FormGroup;
   direccionFormGroup: FormGroup;
   parentescoFormGroup: FormGroup;
+  clienteNatural: IclienteNatural;
 
   constructor(private _formBuilder: FormBuilder,
               private provinciaService: ProvinciasService) {
@@ -43,11 +45,42 @@ export class ClienteNaturalComponent implements OnInit {
       primer_apellido_parentesco: ['', Validators.required],
       segundo_apellido_parentesco: [''],
       celular_parentesco: [''],
+      picker_parentesco: ['']
     });
   }
 
   ngOnInit(): void {
     this.provinciaService.lista_provincias().subscribe(data => console.log(data));
+  }
+
+  getFormClienteNatural() {
+    this.clienteNatural.cedula = this.clienteFormGroup.get('cedula')?.value;
+    this.clienteNatural.codigo = this.clienteFormGroup.get('codigo')?.value;
+    this.clienteNatural.primer_apellido = this.clienteFormGroup.get('primer_apellido')?.value;
+    this.clienteNatural.segundo_apellido = this.clienteFormGroup.get('segundo_apellido')?.value;
+    this.clienteNatural.primer_nombre = this.clienteFormGroup.get('primer_nombre')?.value;
+    this.clienteNatural.segundo_nombre = this.clienteFormGroup.get('segundo_nombre')?.value;
+    this.clienteNatural.celular = this.clienteFormGroup.get('celular')?.value;
+    this.clienteNatural.correo = this.clienteFormGroup.get('correo')?.value;
+    this.clienteNatural.cumpleanios = this.clienteFormGroup.get('cumpleanos')?.value;
+
+    this.clienteNatural.provincia_id = this.direccionFormGroup.get('provincia_id')?.value;
+    this.clienteNatural.ciudad_id = this.direccionFormGroup.get('ciudad_id')?.value;
+    this.clienteNatural.parroquia_id = this.direccionFormGroup.get('parroquia_id')?.value;
+    this.clienteNatural.direccion_domiciliaria = this.direccionFormGroup.get('direccion_domiciliaria')?.value;
+    this.clienteNatural.direccion_oficina = this.direccionFormGroup.get('direccion_oficina')?.value;
+    this.clienteNatural.telefono = this.direccionFormGroup.get('telefono_convencional')?.value;
+
+    this.clienteNatural.tipo_parentesco_id = this.parentescoFormGroup.get('tipo_parentesco')?.value;
+    this.clienteNatural.primer_nombre_parentesco = this.parentescoFormGroup.get('primer_nombre_parentesco')?.value;
+    this.clienteNatural.segundo_nombre_parentesco = this.parentescoFormGroup.get('segundo_nombre_parentesco')?.value;
+    this.clienteNatural.primer_apellido_parentesco = this.parentescoFormGroup.get('primer_apellido_parentesco')?.value;
+    this.clienteNatural.segundo_apellido_parentesco = this.parentescoFormGroup.get('segundo_apellido_parentesco')?.value;
+    this.clienteNatural.celular_parentesco = this.parentescoFormGroup.get('celular_parentesco')?.value;
+    this.clienteNatural.cumpleanios_parentesco = this.parentescoFormGroup.get('picker_parentesco')?.value;
+
+
+
   }
 
 }
