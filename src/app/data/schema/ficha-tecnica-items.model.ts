8a41e90305bf6c6c006fc10d8918d5dd6bccf62d
Luis Alberto Cevallos Cavero <lcevallo@gmail.com>
2021-04-10 18:31:19 -0500

    Paso 1
    Agregar las clases y los servicios a usar

---
+++ src/app/data/schema/ficha-tecnica-items.model.ts
@@ -0,0 +1,17 @@
+export class FichaTecnicaItems {
+  id = 0;
+  fk_ficha_tecnica = 0;
+  factura = '';
+  fecha_mantenimiento = '';
+  recibo = '';
+  ficha_tecnica ?= '';
+  descripcion ?= '';
+  persona_recepta ?= '';
+  firma_url = '';
+  cedula_receptor = '';
+  persona_dio_mantenimiento = '';
+  cedula_dio_mantenimiento = '';
+  created_at = '';
+  updated_at = '';
+  publish = true;
+}
---
+++ src/app/data/schema/ficha-tecnica.model.ts
@@ -0,0 +1,13 @@
+export class FichaTecnica {
+  id = 0;
+  fk_cliente = 0;
+  tipo_cliente = '';
+  codigo = '';
+  tds ?= 0;
+  ppm ?= 0;
+  visitas ?= 0;
+  fecha_comprado = '';
+  created_at ?= '';
+  updated_at ?= '';
+  publish = true;
+}
